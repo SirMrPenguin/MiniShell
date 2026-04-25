9c11160a17a8d417cf97ea490f22a39086ed9f61
anisabel <anafon159@gmail.com>
2026-04-25 17:49:40 +0100

    Delete TUYDI directory

--- TUYDI/parse/validate_tokens.c
+++
@@ -1,5 +0,0 @@
-	if (line[len - 1] == '<' || line[len - 1] == '>' || line[len - 1] == '|' || line[0] == '|')
-	{ 
-		//print erro (syntax error near unexpected token `newline');
-		return (false);
-	}
